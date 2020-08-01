6a7a25a8d0f5b450b705a325bfa31694e752b40b
Sheraff <fpellet@ensc.fr>
2020-08-02 00:49:28 +0200

    basic boids behavior
    some math errors
    napkin sketch behavior
    no offscreenCanvas

---
+++ boid.js
@@ -0,0 +1,292 @@
+const BOUND = 10
+
+export default class Boid {
+	constructor({
+		angle = 0,
+		x = 0,
+		y = 0,
+		color = 'green'
+	}) {
+		this.angle = angle // > 0; < 2𝜋
+		this.x = x
+		this.y = y
+		this.color = color
+		this.size = 20
+		this.width = .6 // radians
+		this.vision = {
+			radius: 100,
+			radians: 3 / 2 * Math.PI // > 0; < 2𝜋
+		}
+		this.speed = Math.random() + 2
+		this.wearsHat = false
+		this.rotation = 0
+	}
+
+	set angle(value) {
+		if(value < 0)
+			this._angle = value % (Math.PI * 2) + Math.PI * 2
+		else
+			this._angle = value % (Math.PI * 2)
+
+	}
+
+	get angle() {
+		return this._angle
+	}
+
+	draw(ctx, { withField } = {}) {
+		const preserveFill = ctx.fillStyle
+
+		if(withField)
+			this.drawFieldOfView(ctx)
+
+		if(this.wearsHat)
+			this.drawHat(ctx)
+
+		this.drawTriangle(ctx)
+		ctx.fillStyle = preserveFill
+	}
+
+	drawHat(ctx) {
+		ctx.fillStyle = this.color
+		ctx.beginPath()
+		ctx.moveTo(this.x, this.y)
+		ctx.arc(
+			this.x,
+			this.y,
+			this.size / 4,
+			0,
+			2 * Math.PI,
+		)
+		ctx.fill()
+	}
+
+	drawTriangle(ctx) {
+		ctx.fillStyle = this.color
+		ctx.beginPath()
+		ctx.moveTo(this.x, this.y)
+		ctx.lineTo(
+			this.x + Math.sin(this.angle + this.width / 2) * this.size,
+			this.y + Math.cos(this.angle + this.width / 2) * this.size,
+		)
+		ctx.lineTo(
+			this.x + Math.sin(this.angle - this.width / 2) * this.size,
+			this.y + Math.cos(this.angle - this.width / 2) * this.size,
+		)
+		ctx.fill()
+	}
+
+	drawFieldOfView(ctx) {
+		ctx.globalAlpha = .2
+		ctx.fillStyle = this.color
+		ctx.beginPath()	
+
+		ctx.moveTo(this.x, this.y)
+		ctx.arc(
+			this.x,
+			this.y,
+			this.vision.radius,
+			- this.angle + this.vision.radians / 2 - Math.PI / 2,
+			- this.angle - this.vision.radians / 2 - Math.PI / 2,
+			true
+		)
+		ctx.moveTo(this.x, this.y)
+		ctx.fill()
+		ctx.globalAlpha = 1
+	}
+
+	update({points, box}) {
+
+		let x = this.x
+		let y = this.y
+
+		adjust: {
+			this.rotation = this.rotation * .9
+
+			if(box) {
+				const wall = this.testWallVisibility(box)
+				if(wall) {
+					this.rotation += Math.sign(wall.angle || 1) / wall.distance
+					break adjust
+				}
+			}
+
+			if(points) {
+				const tooClose = this.findClosest(points)
+				if(tooClose) {
+					// console.log('too close')
+					this.rotation += tooClose / 10
+					// break adjust
+				}
+				const result = this.findGroupDirection(points)
+				if(result && result.count > 4) {
+					// console.log('mimic group direction')
+					this.rotation += Math.sign(result.angle) / 20
+					// break adjust
+				}
+				const direction = this.findDensityDirection(points)
+				if(direction) {
+					// console.log('get to group')
+					this.rotation += direction / 30
+					// break adjust
+				}
+			}
+		}
+
+		this.rotation = Math.sign(this.rotation) * Math.min(Math.PI / 15, Math.abs(this.rotation))
+		this.angle += this.rotation
+
+		x -= Math.sin(this.angle) * this.speed
+		y -= Math.cos(this.angle) * this.speed
+
+		if(box) {
+			if(x < BOUND) x = BOUND
+			if(y < BOUND) y = BOUND
+			if(x > box.width - BOUND) x = box.width - BOUND
+			if(y > box.height - BOUND) y = box.height - BOUND
+		}
+
+		this.x = x
+		this.y = y
+	}
+
+	findClosest(points) {
+		const closestLeft = points.filter(point => {
+			if(point === this)
+				return false
+			const distance = Math.sqrt((point.x - this.x)**2 + (point.y - this.y)**2)
+			return distance < this.size*2
+				&& this.testPointVisibility({x: point.x, y: point.y, side: 'left'})
+		})
+		const closestRight = points.filter(point => {
+			if(point === this)
+				return false
+			const distance = Math.sqrt((point.x - this.x)**2 + (point.y - this.y)**2)
+			return distance < this.size*2
+				&& this.testPointVisibility({x: point.x, y: point.y, side: 'right'})
+		})
+
+		if(!closestLeft.length && !closestRight.length)
+			return false
+
+		return Math.sign(closestRight.length - closestLeft.length)
+	}
+
+	findGroupDirection(points) {
+		const inView = points.filter(point => 
+			point !== this 
+			&& this.testPointVisibility(point)
+		)
+
+		if(!inView.length)
+			return false
+
+		// biased against extremums (because of circular distribution of radians)
+		const atan2X = inView.reduce((sum, {angle}) => sum + Math.sin(angle), 0) / inView.length
+		const atan2Y = inView.reduce((sum, {angle}) => sum + Math.cos(angle), 0) / inView.length
+		const realAngleMean = Math.atan2(atan2X, atan2Y)
+		const angleMinus = realAngleMean - this.angle
+		const anglePlus = (realAngleMean + Math.PI *2) - this.angle
+		const angle = Math.abs(angleMinus) < Math.abs(anglePlus) ? angleMinus : anglePlus
+		return { angle, count: inView.length }
+	}
+
+	findDensityDirection(points) {
+		const leftView = points.filter(point => 
+			point !== this 
+			&& Math.sqrt((point.x - this.x)**2 + (point.y - this.y)**2) > 2*this.size
+			&& this.testPointVisibility({x: point.x, y: point.y, side: 'left'})
+		)
+		const rightView = points.filter(point => 
+			point !== this 
+			&& Math.sqrt((point.x - this.x)**2 + (point.y - this.y)**2) > 2*this.size
+			&& this.testPointVisibility({x: point.x, y: point.y, side: 'right'})
+		)
+		
+		if(!leftView.length && !rightView.length)
+			return false
+
+		return Math.sign(leftView.length - rightView.length)
+	}
+
+	testWallVisibility({height, width}) {
+		const futureX = this.x - Math.sin(this.angle) * this.vision.radius
+		const futureY = this.y - Math.cos(this.angle) * this.vision.radius
+
+		const returns = []
+		if(futureX < BOUND) { // left
+			returns.push({
+				angle: this.angle / (Math.PI / 2) - 1,
+				distance: this.x
+			})
+		}
+		if (futureX > width - BOUND) { // right
+			returns.push({
+				angle: this.angle / (Math.PI / 2) + 1,
+				distance: width - this.x
+			})
+		}
+		if (futureY < BOUND) { // top
+			returns.push({
+				angle: this.angle / (Math.PI / 2),
+				distance: this.y
+			})
+		}
+		if (futureY > height - BOUND) { // bottom
+			returns.push({
+				angle: this.angle / (Math.PI / 2) - 2,
+				distance: height - this.y
+			})
+		}
+
+		if(returns.length === 0)
+			return false
+
+		// cheat
+		if(this.x < BOUND * 10 && this.x < BOUND * 10) {
+			return {
+				angle: this.rotation, 
+				distance: Math.min(Math.abs(this.x - BOUND), Math.abs(this.y - BOUND))
+			}
+		}
+		
+		returns.sort((a, b) => {
+			if (Math.abs(a.distance - b.distance) > BOUND * 2)
+				return a.distance > b.distance
+			else
+				return Math.abs(a.angle - this.rotation) < Math.abs(b.angle - this.rotation)
+			
+		})
+
+		return returns[0]
+	}
+
+	testPointVisibility({x, y, side}) {
+		// is point (x,y) in cone of vision (radius,radians) of boid (this)
+		const dx = this.x - x
+		const dy = this.y - y
+
+		const distance = Math.sqrt(dx**2 + dy**2)
+		if(distance > this.vision.radius)
+			return false
+
+		const angle = this.angleFromDeltas({dx, dy})
+		const deltaAngle = (Math.PI * 2 - this.angle + angle) % (Math.PI * 2)
+
+		if(side === 'left' && deltaAngle > this.vision.radians / 2) {
+			return false
+		} else if (side === 'right' && deltaAngle < Math.PI * 2 - this.vision.radians / 2) {
+			return false
+		} else if(!side && deltaAngle > this.vision.radians / 2 && deltaAngle < Math.PI * 2 - this.vision.radians / 2) {
+			return false
+		}
+
+		return true
+	}
+
+	angleFromDeltas({dx, dy}) {
+		return Math.atan(dx / dy) 
+			+ (dy < 0 ? Math.PI : 0) 
+			+ (dy > 0 && dx < 0 ? Math.PI * 2 : 0)
+	}
+}
---
+++ script.js
@@ -0,0 +1,209 @@
+import COLORS from './colors.js'
+import Boid from './boid.js'
+
+const canvas = document.createElement('canvas')
+canvas.height = 1000
+canvas.width = 1000
+document.body.appendChild(canvas)
+
+const ctx = canvas.getContext('2d')
+
+const cursor = new Boid({
+	x: 450,
+	y: 100,
+	angle: 0,
+	color: 'purple'
+})
+const boids = []
+
+for (let index = 0; index < 100; index++) {
+	const boid = new Boid({
+		x: Math.random() * ctx.canvas.width,
+		y: Math.random() * ctx.canvas.height,
+		angle: Math.random() * Math.PI * 2,
+		color: COLORS[Math.floor(Math.random() * COLORS.length)]
+	})
+	boids.push(boid)
+}
+
+// {
+// 	boids.push(new Boid({
+// 		x: 200,
+// 		y: 200,
+// 		angle: .1,
+// 		color: 'orange'
+// 	}))
+// 	boids.push(new Boid({
+// 		x: 220,
+// 		y: 200,
+// 		angle: .1,
+// 		color: 'orange'
+// 	}))
+// 	boids.push(new Boid({
+// 		x: 200,
+// 		y: 220,
+// 		angle: .1,
+// 		color: 'orange'
+// 	}))
+// 	boids.push(new Boid({
+// 		x: 220,
+// 		y: 220,
+// 		angle: .1,
+// 		color: 'orange'
+// 	}))
+// 	boids.push(new Boid({
+// 		x: 210,
+// 		y: 240,
+// 		angle: .1,
+// 		color: 'orange'
+// 	}))
+
+// 	boids.push(new Boid({
+// 		x: 600,
+// 		y: 600,
+// 		angle: Math.PI,
+// 		color: 'green'
+// 	}))
+// 	boids.push(new Boid({
+// 		x: 620,
+// 		y: 600,
+// 		angle: Math.PI,
+// 		color: 'green'
+// 	}))
+// 	boids.push(new Boid({
+// 		x: 600,
+// 		y: 620,
+// 		angle: Math.PI,
+// 		color: 'green'
+// 	}))
+// 	boids.push(new Boid({
+// 		x: 620,
+// 		y: 620,
+// 		angle: Math.PI,
+// 		color: 'green'
+// 	}))
+// 	boids.push(new Boid({
+// 		x: 610,
+// 		y: 640,
+// 		angle: Math.PI,
+// 		color: 'green'
+// 	}))
+
+// 	boids.push(new Boid({
+// 		x: 200,
+// 		y: 600,
+// 		angle: Math.PI * 3/2,
+// 		color: 'green'
+// 	}))
+// 	boids.push(new Boid({
+// 		x: 220,
+// 		y: 600,
+// 		angle: Math.PI * 3/2,
+// 		color: 'green'
+// 	}))
+// 	boids.push(new Boid({
+// 		x: 200,
+// 		y: 620,
+// 		angle: Math.PI * 3/2,
+// 		color: 'green'
+// 	}))
+// 	boids.push(new Boid({
+// 		x: 220,
+// 		y: 620,
+// 		angle: Math.PI * 3/2,
+// 		color: 'green'
+// 	}))
+// 	boids.push(new Boid({
+// 		x: 210,
+// 		y: 640,
+// 		angle: Math.PI * 3/2,
+// 		color: 'green'
+// 	}))
+
+// 	boids.push(new Boid({
+// 		x: 600,
+// 		y: 200,
+// 		angle: Math.PI * 1/2,
+// 		color: 'green'
+// 	}))
+// 	boids.push(new Boid({
+// 		x: 620,
+// 		y: 200,
+// 		angle: Math.PI * 1/2,
+// 		color: 'green'
+// 	}))
+// 	boids.push(new Boid({
+// 		x: 600,
+// 		y: 220,
+// 		angle: Math.PI * 1/2,
+// 		color: 'green'
+// 	}))
+// 	boids.push(new Boid({
+// 		x: 620,
+// 		y: 220,
+// 		angle: Math.PI * 1/2,
+// 		color: 'green'
+// 	}))
+// 	boids.push(new Boid({
+// 		x: 610,
+// 		y: 240,
+// 		angle: Math.PI * 1/2,
+// 		color: 'green'
+// 	}))
+// }
+
+let lastX, lastY
+canvas.addEventListener('mousemove', ({x, y}) => {
+	lastX = x
+	lastY = y
+})
+
+function drawPoint(ctx, x, y, color = 'black') {
+	const size = 3
+	ctx.fillStyle = color
+	ctx.beginPath()
+	ctx.moveTo(x, y)
+	ctx.arc(
+		x,
+		y,
+		size,
+		0,
+		2 * Math.PI,
+	)
+	ctx.fill()
+}
+
+// function update() {
+// 	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+// 	const boid = boids[1]
+// 	boid.draw(ctx, { withField: true })
+// 	for (let i = 0; i < Math.PI * 2; i += .1) {
+// 		const x = boid.x - Math.sin(i) * (boid.vision.radius - 1)
+// 		const y = boid.y - Math.cos(i) * (boid.vision.radius - 1)
+// 		const visible = boid.testPointVisibility({x, y})
+// 		drawPoint(ctx, x, y, visible ? 'green' : 'red')
+// 	}
+// 	boid.angle += .01
+// }
+
+loop()
+// // update(ctx)
+
+function update(ctx) {
+	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+	boids.forEach(boid => {
+		boid.update({points: [...boids, cursor], box: ctx.canvas})
+		boid.draw(ctx)
+	})
+	cursor.x = lastX
+	cursor.y = lastY
+	cursor.update({points: boids, box: ctx.canvas})
+	cursor.draw(ctx, { withField: true })
+}
+
+function loop() {
+	requestAnimationFrame(() => {
+		update(ctx)
+		loop()
+	})
+}
